83c181a1e0375a30545f4fc5f7f92cbec6f3488e
Nikhil-Jaiz <nikhiljaiswal20@gmail.com>
2024-05-21 22:21:01 +0530

    deleted some code

--- pages/views.py
+++ pages/views.py
@@ -111,7 +111,7 @@ def forgot_password(request):
         # print(token)
         subject = 'Reset Password'
         message = f'Set your new password: {reset_url}'
-        from_email = 'placementcellasiet@outlook.com'
+        from_email = 'nikhiljaiz21@gmail.com'
         recipient_list = [user.email]
         try:
             send_mail(subject, message, from_email, recipient_list, html_message=message)
@@ -375,7 +375,7 @@ def drivecreate(request):
         <html>
         <body>
         <p>Hello,</p>
-        <p>A new drive '{drive_name}' has been created by Placement Cell, ASIET.</p>
+        <p>A new drive '{drive_name}' has been created by Placement Cell, AKGEC.</p>
         <p>Drive details:</p>
         <ul>
         <li>Role: {drive_role}</li>
@@ -390,16 +390,11 @@ def drivecreate(request):
         </body>
         </html>
         """
-        from_email = 'placementcellasiet@outlook.com'
+        from_email = 'nikhiljaiz21@gmail.com'
         recipient_list = [user.email for user in User.objects.all()]
         send_mail(subject, message, from_email, recipient_list, html_message=message)
 
-        # subject = 'New Placement drive details'
-        # user=User.objects.all()
-        # message = f"Hello,\n\nA new drive '{drive_name}' has been created by Placement Cell, ASIET.\n\nDrive details:-\n\nRole: {drive_role}\nPackage: {drive_pckg}\nDate: {drive_date}\nBacklogs: {drive_bklgs}\nCGPA: {drive_cgpa}\n\nDescription: {drive_desc1}"
-        # from_email = 'placementcellasiet@outlook.com'
-        # recipient_list = [user.email for user in User.objects.all()]
-        # send_mail(subject, message, from_email, recipient_list)
+      
         
         return redirect('/cordinator')
     return render(request,'pages/createdrive.html',context)
@@ -578,7 +573,7 @@ def contact(request):
         from_email = request.POST.get('email')
         subject = request.POST.get('subject')
         message = request.POST.get('message')
-        recipient_list = ["placementcellasiet@outlook.com"]
+        recipient_list = ["nikhiljaiz21@gmail.com"]
         send_mail(subject, message, from_email, recipient_list, html_message=message)
         return redirect('http://127.0.0.1:8000/')
     return render(request,'pages/home.html')
