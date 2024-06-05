0a95b4ed6af06088d5db558a28b89385f7bcc61e
Nikhil-Jaiz <nikhiljaiswal20@gmail.com>
2024-06-06 01:23:57 +0530

    Update

--- pages/admin.py
+++ pages/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from .models import Profile
 from .models import Drives
 from .models import Student
+from .models import Placedstudent
 admin.site.register(Profile)
 admin.site.register(Drives)
 admin.site.register(Student)
+admin.site.register(Placedstudent)
 # Register your models here.
